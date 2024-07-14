2cb6290e082cbe9c49156057d9b63365f579574d
comalnik <gostencnikluka@gmail.com>
2024-07-14 02:17:38 +0200

    edits

--- .install.sh
+++ .install.sh
@@ -1,4 +1,5 @@
 find "$HOME/dotfiles/.scripts/" -type f -exec chmod +x {} \;
 find "$HOME/dotfiles" -type f -name "gtk.sh" -exec chmod +x {} \;
 cp "$HOME/dotfiles/.zshrc" "$HOME/.zshrc"
+/bin/rm $HOME/dotfiles/README.md
 $HOME/dotfiles/.scripts/rofi/rofi_themes

b1b8a75667566665fbaf1fc32d08a9580559efae
Comalnik <gostencnikluka@proton.me>
2024-07-28 01:12:11 +0000

    Update gtk.sh

--- nord-simple/gtk.sh
+++ nord-simple/gtk.sh
@@ -5,7 +5,7 @@ gsettings set org.gnome.desktop.interface gtk-theme "Nordic"
 gsettings set org.gnome.desktop.wm.preferences theme "Nordic"
 
 # Set font
-gsettings set org.gnome.desktop.interface font-name "JetBrains Mono Nerd 11"
+gsettings set org.gnome.desktop.interface font-name "JetBrainsMono Nerd Font 11"
 # Set cursor theme
 gsettings set org.gnome.desktop.interface cursor-theme "Nordzy-cursors"
 

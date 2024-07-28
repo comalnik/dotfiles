baf9888d4e2122230f54cdd6b9b6c27335475567
Comalnik <gostencnikluka@proton.me>
2024-07-28 01:10:56 +0000

    Update gtk.sh

--- forest-light/gtk.sh
+++ forest-light/gtk.sh
@@ -5,8 +5,7 @@ gsettings set org.gnome.desktop.interface gtk-theme "Everforest-Green-Dark"
 gsettings set org.gnome.desktop.wm.preferences theme "Everforest-Green-Dark"
 
 # Set font
-gsettings set org.gnome.desktop.interface font-name "Maple Mono 11"
-
+gsettings set org.gnome.desktop.interface font-name "Maple Mono NF 11"
 # Set cursor theme
 gsettings set org.gnome.desktop.interface cursor-theme "BreezeX-RosePine-Linux"
 
